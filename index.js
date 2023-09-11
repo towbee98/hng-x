4943387c39dbc49655c2188e37370865bb48b54a
Oladele Tobiloba <tobiemma200@gmail.com>
2023-09-11 06:09:29 +0100

    updated the file

--- index.js
+++ index.js
@@ -4,17 +4,17 @@ const express = require("express");
 const PORT = process.env.PORT;
 const app = express();
 app.use(express.json());
-app.get("/", (req, res, next) => {
-  const { slack_name, track } = req.query;
 
+app.get("/hello", (req, res, next) => {
+  const { slack_name, track } = req.query;
   return res.status(200).json({
-    status_code: 200,
+    slack_name,
     current_day: moment().format("dddd"),
     utc_time: moment().utc(),
-    track: "backend",
-    github_file_url: "",
-    github_repo_url: "",
-    slack_name: "Towbee",
+    track,
+    github_file_url: "https://github.com/towbee98/hng-x/blob/main/index.js",
+    github_repo_url: "https://github.com/towbee98/hng-x",
+    status_code: 200,
   });
 });
 app.listen(PORT, () => {
