37b4c84efa02558243be118dc1fd416e4f58ed32
Oladele Tobiloba <tobiemma200@gmail.com>
2023-09-11 06:18:35 +0100

    added an error handler for wrong endpoints

--- index.js
+++ index.js
@@ -5,17 +5,20 @@ const PORT = process.env.PORT;
 const app = express();
 app.use(express.json());
 
-app.get("/hello", (req, res, next) => {
+app.get("/api", (req, res, next) => {
   const { slack_name, track } = req.query;
   return res.status(200).json({
-    slack_name,
+    slack_name: slack_name || "",
     current_day: moment().format("dddd"),
     utc_time: moment().utc(),
-    track,
+    track: track || "",
     github_file_url: "https://github.com/towbee98/hng-x/blob/main/index.js",
     github_repo_url: "https://github.com/towbee98/hng-x",
     status_code: 200,
   });
+});
+app.get("*", (req, res, next) => {
+  res.status(404).json({ status_code: 404, message: "page not found" });
 });
 app.listen(PORT, () => {
   console.log("Server running at", PORT);
